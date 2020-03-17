0e332ab20f8aed0b2b10bec1cef83d8d968513cb
Gustavo Claure <gzdncs@gmail.com>
2020-03-16 21:45:32 -0400

    Add react-navigation for android, missing, tabs, drawer & stack

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>Soy App!</Text>
+      </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ebebeb',
-  },
-  text: {
-    color: '#101010',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
 
 export default App;
