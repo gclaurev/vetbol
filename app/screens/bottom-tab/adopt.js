9b4730582b4329b87cdf3820ea96b38ccd637ad8
Gustavo Claure <gzdncs@gmail.com>
2020-03-29 22:08:13 -0400

    replace adopt and foun screens, needs updates Signed-off-by: Gustavo Claure <gclaurev@gmail.com>

--- app/screens/bottom-tab/adopt.js
+++ app/screens/bottom-tab/adopt.js
@@ -1,12 +1,210 @@
-import React from 'react';
-import {SafeAreaView, View, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Text,
+  Platform,
+  ScrollView,
+  Image,
+} from 'react-native';
+import {Button, Overlay} from 'react-native-elements';
+
+//Storing
+import {storeOneForLists, getKey} from '../../services/database';
+
+//Appearance
+import styles from '../../styling/vetBolStyles';
+import {colors} from '../../styling/colors';
+
+//Date Picker
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+//Image picker
+import ImagePicker from 'react-native-image-picker';
 
 export default function Adpot() {
-  return (
-    <SafeAreaView>
-      <View>
-        <Button title="press me n" />
-      </View>
-    </SafeAreaView>
+//form
+const [name, setName] = useState(false);
+const [desc, setDesc] = useState(false);
+const [whatsApp, setWhatsApp] = useState(false);
+const [address, setAddress] = useState(false);
+
+//overlay
+const [visible, setVisible] = useState(false);
+
+function toggle() {
+  setVisible(prevState => !prevState);
+}
+
+function finish() {
+  const id = getKey();
+  storeOneForLists(
+    id,
+    'adopt',
+    {
+      name,
+      desc,
+      whatsApp,
+      address,
+      date,
+    },
+    image,
   );
+  setName(false);
+  setDesc(false);
+  setWhatsApp(false);
+  setAddress(false);
+  setVisible(false);
+  setShow(false);
+  setImage(false);
+}
+
+// datepicker
+const [date, setDate] = useState(new Date());
+const [show, setShow] = useState(false);
+
+const onChange = (event, selectedDate) => {
+  const currentDate = selectedDate || date;
+  setShow(Platform.OS === 'ios');
+  setDate(currentDate);
+  console.log(currentDate);
+  console.log(date);
+};
+
+const showMode = () => {
+  setShow(!show);
+};
+
+//Image picker
+const [image, setImage] = useState(false);
+
+const selectPhotoTapped = () => {
+  const options = {
+    quality: 1.0,
+    maxWidth: 500,
+    maxHeight: 500,
+    storageOptions: {
+      skipBackup: true,
+    },
+  };
+
+  ImagePicker.showImagePicker(options, response => {
+    console.log('Response = ', response);
+
+    if (response.didCancel) {
+      console.log('User cancelled image picker');
+    } else if (response.error) {
+      console.log('ImagePicker Error: ', response.error);
+    } else if (response.customButton) {
+      console.log('User tapped custom button: ', response.customButton);
+    } else {
+      const source = {uri: response.uri};
+      setImage(source);
+    }
+  });
+};
+
+return (
+  <SafeAreaView style={styles.safeArea}>
+    <Overlay isVisible={visible}>
+      <View style={styles.container}>
+        <Text style={styles.overlayTitle}>🐶💛 NUEVO PERDIDO 💛🐱</Text>
+        <TouchableOpacity
+          style={styles.closeOverlay}
+          onPress={toggle}
+          onBackdropPress={toggle}>
+          <Text style={styles.closeButtonText}>+</Text>
+        </TouchableOpacity>
+        <ScrollView>
+          <Text style={styles.overlayLabel}>Nombre del peludito:</Text>
+          <TextInput
+            style={styles.overlayTextInput}
+            selectionColor={colors.brown}
+            placeholder="Anvorgesito"
+            onChangeText={val => setName(val)}
+          />
+          <Text style={styles.overlayLabel}>Descripción:</Text>
+          <TextInput
+            style={styles.overlayTextInput}
+            selectionColor={colors.brown}
+            placeholder="Negro con cola blanca"
+            onChangeText={val => setDesc(val)}
+          />
+          <Text style={styles.overlayLabel}>WhatsApp:</Text>
+          <TextInput
+            style={styles.overlayTextInput}
+            selectionColor={colors.brown}
+            placeholder="60977768"
+            onChangeText={val => setWhatsApp(val)}
+          />
+          <Text style={styles.overlayLabel}>Dirección:</Text>
+          <TextInput
+            style={styles.overlayTextInput}
+            selectionColor={colors.brown}
+            placeholder="Av. Beni esq. 4to anillo..."
+            onChangeText={val => setAddress(val)}
+          />
+          <Text style={styles.overlayLabel}>Fecha:</Text>
+          <View>
+            <Button
+              buttonStyle={styles.overlayDateButton}
+              titleStyle={styles.overlayDateButtonTitle}
+              onPress={showMode}
+              title="Seleccionar Fecha"
+            />
+          </View>
+          {show && (
+            <DateTimePicker
+              testID="dateTimePicker"
+              timeZoneOffsetInMinutes={0}
+              value={date}
+              mode="date"
+              is24Hour={true}
+              display="default"
+              onChange={onChange}
+              maximumDate={new Date()}
+            />
+          )}
+
+          <TouchableOpacity
+            style={styles.pictureContainer}
+            onPress={selectPhotoTapped}>
+            <View
+              style={[
+                styles.avatar,
+                styles.avatarContainer,
+                {marginBottom: 5},
+              ]}>
+              {image === false ? (
+                <Text style={styles.overlayDateButtonTitle}>
+                  Click para subir foto
+                </Text>
+              ) : (
+                <Image style={styles.avatar} source={image} />
+              )}
+            </View>
+          </TouchableOpacity>
+        </ScrollView>
+        <View>
+          <Button
+            buttonStyle={styles.overlayFinishButton}
+            titleStyle={styles.overlayDateButtonTitle}
+            onPress={finish}
+            title="Finalizar registro"
+          />
+        </View>
+      </View>
+    </Overlay>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={toggle}
+        onBackdropPress={toggle}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  </SafeAreaView>
+);
 }
--- app/screens/bottom-tab/found.js
+++ app/screens/bottom-tab/found.js
@@ -1,6 +1,28 @@
-import React from 'react';
-import {SafeAreaView, Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Text,
+  Platform,
+  ScrollView,
+  Image,
+} from 'react-native';
+import {Button, Overlay} from 'react-native-elements';
 
+//Storing
+import {storeOneForLists, getKey} from '../../services/database';
+
+//Appearance
+import styles from '../../styling/vetBolStyles';
+import {colors} from '../../styling/colors';
+
+//Date Picker
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+//Image picker
+import ImagePicker from 'react-native-image-picker';
 export default function Found() {
   return (
     <SafeAreaView>
