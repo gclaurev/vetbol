0f8ca69a056f0d935cc42843fedb1ae7ddc47d51
Gustavo Claure <gclaurev@gmail.com>
2020-06-24 20:09:19 -0400

    Faceboo login working in android. Need styling Signed-off-by: Gustavo Claure <gclaurev@gmail.com>

--- app/services/login/facebook.js
+++ app/services/login/facebook.js
@@ -1,52 +1,108 @@
-import React from 'react';
-import {Button} from 'react-native';
+import React, {useState} from 'react';
+import {Button, View} from 'react-native';
 import auth from '@react-native-firebase/auth';
-import {LoginManager, AccessToken} from 'react-native-fbsdk';
-
-async function onFacebookButtonPress() {
-  
-  console.log('get in1');
-
-  // Attempt login with permissions
-  const result = await LoginManager.logInWithPermissions([
-    'public_profile',
-    'email',
-  ]);
-  console.log('get in2');
-
-  if (result.isCancelled) {
-    throw 'User cancelled the login process';
-  }
-
-  console.log('get in3');
-  // Once signed in, get the users AccesToken
-  const data = await AccessToken.getCurrentAccessToken();
-
-  console.log('get in4');
-  if (!data) {
-    throw 'Something went wrong obtaining access token';
-  }
-  console.log('get in5');
-
-  // Create a Firebase credential with the AccessToken
-  const facebookCredential = auth.FacebookAuthProvider.credential(
-    data.accessToken,
-  );
-
-  console.log('get in6');
-  // Sign-in the user with the credential
-  return auth().signInWithCredential(facebookCredential);
-}
+import {
+  LoginManager,
+  AccessToken,
+  GraphRequest,
+  GraphRequestManager,
+} from 'react-native-fbsdk';
 
 export default function FacebookSignIn() {
+  const [token, setToken] = useState(false);
+
+  async function onFacebookButtonPress() {
+    // Attempt login with permissions
+    const result = await LoginManager.logInWithPermissions([
+      'public_profile',
+      'email',
+    ]);
+
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+    // Once signed in, get the users AccesToken
+    const data = await AccessToken.getCurrentAccessToken();
+
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+
+    // Create a Firebase credential with the AccessToken
+    const facebookCredential = auth.FacebookAuthProvider.credential(
+      data.accessToken,
+    );
+
+    setToken(data.accessToken);
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(facebookCredential);
+  }
+
+  async function loging() {
+    AccessToken.getCurrentAccessToken().then(data => {
+      let accessToken = data.accessToken;
+      alert(accessToken.toString());
+
+      const responseInfoCallback = (error, result) => {
+        if (error) {
+          console.log(error);
+          alert('Error fetching data: ' + error.toString());
+        } else {
+          console.log(result);
+
+          // Here's my code
+          alert(
+            'Success fetching data: ' +
+              result.name.toString() +
+              ', ' +
+              result.email.toString(),
+          );
+          /*
+            if(your DB already got this email or something unique) {
+              // SignIn()
+            }
+            // when your DB doesn't have this email
+            else {
+              // Do signUp() with this infomation and SignIn()
+            }
+            */
+        }
+      };
+
+      const infoRequest = new GraphRequest(
+        '/me',
+        {
+          accessToken: accessToken,
+          parameters: {
+            fields: {
+              string: 'email,name,first_name,middle_name,last_name',
+            },
+          },
+        },
+        responseInfoCallback,
+      );
+
+      // Start the graph request.
+      new GraphRequestManager().addRequest(infoRequest).start();
+    });
+  }
+
   return (
-    <Button
-      title="Facebook Sign-In"
-      onPress={() =>
-        onFacebookButtonPress().then(() =>
-          console.log('Signed in with Facebook!'),
-        )
-      }
-    />
+    <View>
+      <Button
+        title="Facebook Sign-In"
+        onPress={() =>
+          onFacebookButtonPress().then(() =>
+            console.log('Signed in with Facebook!'),
+          )
+        }
+      />
+      <Button
+        title="Facebook Log Out"
+        onPress={() =>
+          loging().then(() => console.log('Signed in with Facebook!'))
+        }
+      />
+    </View>
   );
 }
